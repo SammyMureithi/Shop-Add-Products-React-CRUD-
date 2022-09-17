a68b0684289adc4dcece0477926e4c1856bd3889
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-17 17:31:16 +0300

    AddProduct

---
+++ src/Components/AddProducts.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import ProductsCard from './ProductsCard';
+
+function AddProducts() {
+    const myProducts = [
+        { id: 1, name: "Obama Smoothe", quantity: "10", price: "20" },
+        { id: 2, name: "Obama", quantity: "100", price: "120" },
+        { id: 3, name: "Smoothe", quantity: "70", price: "54" },
+    ]
+    const [products, setProducts] = useState(myProducts);
+    const [productName, setProductName] = useState();
+    const [price, SetPrice] = useState();
+    const [quantity, setQuantity] = useState();
+    const getNextId = ( ( id ) => () => ++id )( 3);
+    function AddnewProduct( e ) {
+        e.preventDefault();
+        let newPro = {
+            id:products.length+1,
+            name: productName,
+            price: price,
+            quantity: quantity
+        }
+        let newProductList = [...products, newPro]
+        console.log( newPro );
+        setProducts( newProductList );
+        setProductName( "" );
+        setQuantity( "" );
+        SetPrice( "" );
+    }
+    function handleChange( e ) {
+        if ( e.target.name === "productName" ) {
+            setProductName( e.target.value );
+        }
+        if ( e.target.name === "quantity" ) {
+            setQuantity( e.target.value );
+        }
+        if ( e.target.name === "price" ) {
+            SetPrice( e.target.value );
+        }
+    }
+    const prod = products.map( element => {
+      return   <ProductsCard title={element.name} price={element.price} quantity={element.quantity}/>
+    })
+  return (
+      <div>
+          <form>
+              <h1>Add Products</h1>
+              <label htmlFor='productName'>Product Name</label>
+              <input type="text" id='productName' name='productName'
+                  value={productName} placeholder="Enter Product Name..." onChange={handleChange}/>
+              <label htmlFor='quantity'>Quantity</label>
+              <input type="number" id='quantity' name='quantity' placeholder="Enter Quantity..."
+                  onChange={handleChange} value={quantity} />
+              <label htmlFor='price'>Price</label>
+              <input type="text" id='price' name='price'
+                  placeholder="Enter Price..." onChange={handleChange} value={price} />
+              <button type='submit' onClick={AddnewProduct}>Add Product</button>
+              <h1>Total:::{products.reduce((total,currentElement)=> total+=currentElement.price*currentElement.quantity,0)}</h1>
+          </form>
+          <div id='purchase'>
+          {prod}
+          </div>
+          
+    </div>
+  )
+}
+
+export default AddProducts
---
+++ src/Components/ProductsCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProductsCard(props) {
+  return (
+      <div id="product">
+          <div id="desc">
+          <h1>{`Products: ${props.title}`}</h1>
+          <h1>{`Quantity: ${props.quantity}`}</h1>
+          <h1>{`Price: ${props.price}`}</h1>
+          </div>
+          
+          <div id="products-btn">
+              <button id="update">Update</button>
+              <button id="delete">Delete</button>
+          </div>
+    </div>
+  )
+}
+
+export default ProductsCard
